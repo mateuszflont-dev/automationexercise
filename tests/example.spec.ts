afb11e9c611d0506658777af32b504d155ce8752
mateuszflont-dev <flont.mateusz92@gmail.com>
2025-12-24 01:31:23 +0100

    first exercise

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+import userData from '../testUserData.json';
+
 
 test('Register User', async ({ page }) => {
   await page.goto('http://automationexercise.com');
@@ -14,12 +16,42 @@ test('Register User', async ({ page }) => {
   const signupLabel = page.getByRole('heading', {name: "New User Signup!"});
   await expect(signupLabel).toBeVisible();
 
+  const myUser = userData.validUser;
   const signupForm = page.locator('.signup-form');
   const nameTextbox = signupForm.getByRole('textbox', {name: "Name"});
-  await nameTextbox.fill("Tester");
+  await nameTextbox.fill(myUser.name);
   const emailTextbox = signupForm.getByRole('textbox', {name: "Email Address"});
-  await emailTextbox.fill("varkhaim@gmail.com");
+  await emailTextbox.fill(myUser.email);
 
   const signupButton = page.getByRole('button', {name: "Signup"});
   await signupButton.click();
+
+  //---
+
+  const registrationForm = page.locator(".login-form");
+    
+
+    const accountInformationHeader = registrationForm.getByRole('heading', {name: "Enter Account Information"});
+    await expect(accountInformationHeader).toBeVisible();
+
+    const formGenderSection = registrationForm.locator(".clearfix");
+    const formRadio = formGenderSection.getByRole('radio', {name: myUser.gender, exact: true});
+    await formRadio.check();
+
+    const formNameBox = registrationForm.getByRole('textbox', {name: "name"});
+    await formNameBox.fill(myUser.name);
+
+    //const formEmailBox = registrationForm.getByRole('textbox', {name: "Email"});
+
+    const formPasswordBox = registrationForm.getByRole('textbox', {name: "Password"});
+    await formPasswordBox.fill(myUser.password);
+
+    const birthdaySection = registrationForm.locator(".form-group").filter({hasText: "Date of birth"});
+      const daySelector = birthdaySection.getByRole('combobox', {name: "Day"});
+        await daySelector.selectOption(myUser.dateOfBirth.day);
+      const monthSelector = birthdaySection.getByRole('combobox', {name: "Month"});
+        await monthSelector.selectOption(myUser.dateOfBirth.month);
+      const yearSelector = birthdaySection.getByRole('combobox', {name: "Year"});
+        await yearSelector.selectOption(myUser.dateOfBirth.year);
+
 });
